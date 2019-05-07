391b8637e84c7003ba0eb6cef4a8b43da0f14167
jdagdelen <jjdagdelen@gmail.com>
2019-05-06 22:40:31 -0700

    open sites function

--- graph.c
+++ graph.c
@@ -2,17 +2,54 @@
 // Created by John Dagdelen on 4/28/19.
 //
 
-#include </Users/jdagdelen/Dropbox/MP/clusters/graham/igraph/include/igraph.h>
+#include <igraph/igraph.h>
+#include <gsl/gsl_combination.h>
+#include <stdio.h>
+
+
+// Returns the "available" sites for a new vertex to be connected to
+// (sites are available if they have fewer than 6 neighbors)
+void get_open_sites(igraph_t *seed, igraph_vector_t *open){
+    igraph_vector_clear(open);
+    igraph_vector_t degrees;
+    igraph_vector_init(&degrees, igraph_vcount(seed));
+    igraph_degree(seed, &degrees, igraph_vss_all(), IGRAPH_ALL, 0);
+    for(int i=0; i<igraph_vcount(seed); i++) {
+        if(VECTOR(degrees)[i]<6)
+            igraph_vector_push_back(open, i);
+    }
+    igraph_vector_destroy(&degrees);
+//    igraph_vector_print(open);
+}
+
+
+//void get_possible_edge_lists(igraph_t *seed){
+//    gsl_combination * c;
+//    size_t i;
+//    int n = igraph_vcount(seed);
+//    c = gsl_combination_calloc(4, i);
+//}
+
+
+igraph_vector_ptr get_possible_changes(igraph_vector_t *open_sites)
+
+
+void mutate_seed(igraph_t *seed, igraph_vector_ptr_t *results){
+    // get all combinations of up to 6 open vertices to connect new vertex to
+
+
+    igraph_vector_ptr new_edge_vectors;
+
+
+}
+
 
 int main(void) {
-    igraph_integer_t diameter;
     igraph_t graph;
-    igraph_rng_seed(igraph_rng_default(), 42);
-    igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP, 1000, 5.0 / 1000,
-                            IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
-    igraph_diameter(&graph, &diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
-    printf("Diameter of a random graph with average degree 5: %d\n",
-           (int) diameter);
-    igraph_destroy(&graph);
+    igraph_small(&graph, 0, IGRAPH_UNDIRECTED, 0,1, 1,2, 2,3, 3,4, 6,1, -1);
+    igraph_vector_t open;
+    igraph_vector_init(&open, igraph_vcount(&graph));
+    get_open_sites(&graph, &open);
+
     return 0;
 }
--- graph.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by John Dagdelen on 4/28/19.
-//
-
-#ifndef GRAHAM_GRAPH_H
-#define GRAHAM_GRAPH_H
-
-#endif //GRAHAM_GRAPH_H
